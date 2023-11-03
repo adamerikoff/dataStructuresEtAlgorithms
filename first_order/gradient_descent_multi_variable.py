d016630ea7411f0ae4a123ed873bd48f7ca9449b
adamerikoff <adamerikoff@protonmail.com>
2023-11-04 02:11:16 +0600

    change gradient calculation from manual to numerical

--- first_order/gradient_descent_multi_variable.py
+++ first_order/gradient_descent_multi_variable.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 def z_func(x, y):
     return np.sin(5 * x) * np.cos(5 * y) / 5
 
-def calculate_gradient(x, y):
-    return np.cos(5 * x) * np.cos(5 * y), -np.sin(5 * x) * np.sin(5 * y)
+def numerical_gradient_2d(f, x, y, epsilon=1e-5):
+    gradient_x = (f(x + epsilon, y) - f(x - epsilon, y)) / (2 * epsilon)
+    gradient_y = (f(x, y + epsilon) - f(x, y - epsilon)) / (2 * epsilon)
+    return gradient_x, gradient_y
 
 x = np.arange(-1,1,0.005)
 y = np.arange(-1,1,0.005)
@@ -19,8 +21,8 @@ learning_rate = 0.01
 
 ax = plt.subplot(projection="3d", computed_zorder=False)
 
-for _ in range(0, 1000):
-    X_d, Y_d = calculate_gradient(current_position[0], current_position[1])
+for _ in range(0, 100):
+    X_d, Y_d = numerical_gradient_2d(z_func, current_position[0], current_position[1])
     X_n, Y_n = current_position[0] - learning_rate * X_d, current_position[1] - learning_rate * Y_d
     current_position = (X_n, Y_n, z_func(X_n, Y_n))
     ax.plot_surface(X, Y, Z, cmap="viridis")

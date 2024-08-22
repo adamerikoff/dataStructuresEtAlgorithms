62abca27c62ec7af0515ec792ea08cd1c4186d96
Adam Erikuly <adamerikoff@protonmail.com>
2024-08-23 03:46:22 +0500

    refactored function names

--- data_structures/hash_c/src/hash.c
+++ data_structures/hash_c/src/hash.c
@@ -4,41 +4,70 @@
 
 #include "hash.h"
 
-hash_table* hash_table_new() {
-    hash_table* ht = malloc(sizeof(hash_table));
+#define HT_PRIME_1 151
+#define HT_PRIME_2 163
+
+static htable_element HT_DELETED_ITEM = {NULL, NULL};
+
+static htable_element* htable_new_element(const char* key, const char* value) {
+    htable_element* item = malloc(sizeof(htable_element));
+    if (item == NULL) {
+        return NULL;
+    }
+
+    item->key = strdup(key);
+    if (item->key == NULL) {
+        free(item);
+        return NULL;
+    }
+    item->value = strdup(value);
+    if (item->value == NULL) {
+        free(item->key);
+        free(item);
+        return NULL;
+    }
+
+    return item;
+}
+
+htable* htable_new() {
+    htable* ht = malloc(sizeof(htable));
+    if (ht == NULL) {
+        return NULL;
+    }
 
     ht->size  = 53;
     ht->count = 0;
-    ht->items = calloc((size_t)ht->size, sizeof(hash_table_item*));
+    ht->items = calloc((size_t)ht->size, sizeof(htable_element*));
+    if (ht->items == NULL) {
+        free(ht);
+        return NULL;
+    }
 
     return ht;
 }
 
-static hash_table_item* hash_table_new_item(const char* key, const char* value) {
-    hash_table_item* item = malloc(sizeof(hash_table_item));
-    item->key = strdup(key);
-    item->value = strdup(value);
-    return item;
-}
-
-static void hash_table_delete_item(hash_table_item* item) {
+static void htable_delete_element(htable_element* item) {
+    if (item == NULL) {
+        return;
+    }
     free(item->key);
     free(item->value);
     free(item);
 }
 
-void hash_table_delete(hash_table* ht) {
+void htable_delete_table(htable* ht) {
     for (int i =0; i < ht->size; i++) {
-        hash_table_item* item = ht->items[i];
+        htable_element* item = ht->items[i];
         if(item != NULL) {
-            hash_table_delete_item(item);
+            htable_delete_element(item);
         }
     }
     free(ht->items);
     free(ht);
 }
 
-static int hash_table_hash(const char* s, const int prime, const int bucket_size) {
+static int htable_hash(const char* s, const int prime, const int bucket_size) {
     long hash = 0;
     const int len_s = strlen(s);
     for (int i = 0; i < len_s; i++) {
@@ -46,4 +75,54 @@ static int hash_table_hash(const char* s, const int prime, const int bucket_size
         hash = hash % bucket_size;
     }
     return (int)hash;
+}
+
+static int htable_get_hash(const char* s, const int num_buckets, const int attempt) {
+    const int hash_a = htable_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = htable_hash(s, HT_PRIME_2, num_buckets);
+
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+void htable_insert_element(htable* ht, const char* key, const char* value) {
+    htable_element* item = htable_new_element(key, value);
+    int index = htable_get_hash(item->key, ht->size, 0);
+    htable_element* current_item = ht->items[index];
+    int i = 1;
+    while (current_item != NULL) {
+        index = htable_get_hash(item->key, ht->size, i);
+        current_item = ht->items[index];
+        i++;
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
+
+char* htable_search(htable* ht, const char* key) {
+    int index = htable_get_hash(key, ht->size, 0);
+    htable_element* item = ht->items[index];
+    int i = 1;
+    while(item != NULL) {
+        if (strcmp(item->key, key) == 0) {
+            return item->value;
+        }
+        index = htable_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+    return NULL;
+}
+
+void htable_delete(htable* ht, const char* key) {
+    int index = htable_get_hash(key, ht->size, 0);
+    htable_element* item = ht->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (item != &HT_DELETED_ITEM) {
+            if (strcmp(item->key, key) == 0) {
+                htable_delete_element(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+            }
+        }
+    }
 }

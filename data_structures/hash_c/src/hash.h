8f7bd8a3ffc602c67fd802502a358f1e13ab157e
Adam Erikuly <adamerikoff@protonmail.com>
2024-08-22 23:52:10 +0500

    structs creayed

--- data_structures/hash_c/src/hash.h
+++ data_structures/hash_c/src/hash.h
@@ -1,10 +1,10 @@
 typedef struct {
-    char *key;
-    char *value;
+    char* key;
+    char* value;
 } hash_table_item;
 
 typedef struct {
     int size;
     int count;
-    hash_table_item **items;
+    hash_table_item** items;
 } hash_table;

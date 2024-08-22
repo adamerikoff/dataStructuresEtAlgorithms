dd6dc2ee2942dfbefae074cdbae7472cc457b721
Adam Erikuly <adamerikoff@protonmail.com>
2024-08-23 03:57:56 +0500

    renamed functions for hash table in header file

--- data_structures/hash_c/src/hash.h
+++ data_structures/hash_c/src/hash.h
@@ -8,3 +8,9 @@ typedef struct {
     int count;
     htable_element** items;
 } htable;
+
+htable* htable_new();
+void htable_delete_table(htable* ht);
+void htable_insert(htable* ht, const char* key, const char* value);
+char* htable_search(htable* ht, const char* key);
+void htable_delete(htable* ht, const char* key);

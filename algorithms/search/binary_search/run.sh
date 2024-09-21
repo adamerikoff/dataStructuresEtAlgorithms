8f8bfefb68125a94d791007515ec144851450fa3
Adam Erikuly <adamerikoff@protonmail.com>
2024-09-21 15:43:57 +0500

    finished

--- algorithms/search/binary_search/run.sh
+++ algorithms/search/binary_search/run.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-main="binary_sorting.c"
-filename="sorted_numbers.txt"
+main="binary_search.c"
 output="output.o"
 
-
-sequence=$(seq 999999)
-echo "$sequence" > "$filename"
-echo "The numbers are saved in $filename."
-
 if gcc "$main" -o "$output"; then
-    # Check if the random_numbers file exists
-    if [[ -f "$filename" ]]; then
-        # Run the compiled output with the random_numbers as an argument
-        ./"$output" "$filename"
-    else
-        echo "Error: $filename not found."
-        exit 1
-    fi
+    ./"$output"
 else
     echo "Compilation failed."
     exit 1

bb24695d20ed393033720fdf45178ff26088116f
Adam Erikuly <adamerikoff@protonmail.com>
2024-09-21 10:52:11 +0500

    Update binary_sorting.c

--- algorithms/sorting/binary_sorting/binary_sorting.c
+++ algorithms/sorting/binary_sorting/binary_sorting.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 
+void read_lines(char* filename) {
+    FILE * file;
+    char line[256];
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    // Read the file line by line
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+
+    // Close the file
+    fclose(file);
+}
+
+
 int main(int argc, char *argv[]) {
     // Check if an argument is provided
     if (argc > 1) {
-        printf("Hello, %s!\n", argv[1]);  // Greet the argument
+        read_lines(argv[1]);
     } else {
         printf("Hello, World!\n");  // Default greeting
     }
